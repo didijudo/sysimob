f72badecb823320453e7191288e111793a7efe49
Diego Andrade <didi.ufs@gmail.com>
2012-06-01 16:39:34 -0300

    Inicio do layout do site

---
+++ index2.php
@@ -0,0 +1,37 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	 <!-- Framework CSS -->
+    <link rel="stylesheet" href="css/blueprint/screen.css" type="text/css" media="screen, projection"/>
+    <link rel="stylesheet" href="css/blueprint/plugins/buttons/screen.css" type="text/css" media="screen, projection"/>
+    <link rel="stylesheet" href="css/blueprint/print.css" type="text/css" media="print"/>
+    <link rel="stylesheet" href="css/blueprint/plugins/link-icons/screen.css" type="text/css"/>
+    <!--[if lt IE 8]><link rel="stylesheet" href="css/blueprint/ie.css" type="text/css" media="screen, projection"><![endif]-->
+</head>
+    <body>
+    	<div class="container span-20">
+    		<div class="box push-5 span-15 last">
+    			<img src="img/logo.jpg" width="100" height="80" class="span-3"/>
+    			<h3 class="text push-3 prepend-top last">AC - IMOBILIARIO</h3>
+    		</div>
+    		
+    		<div class="boxF push-5 span-15 last">
+    			<a href="{#link1}" class="prepend-2 append-2">Home</a>
+    			<a href="{#link2}" class="append-2">Cadastrar</a>
+    			<a href="{#link3}" class="append-2">Listar</a>
+    			<a href="{#link4}" class="append-2 last">Visitar</a>
+    		</div>
+    		
+    		<div class="box span-15 push-5">
+    			<h3>Corpo</h3>
+    		</div>
+    		
+    		
+    	</div>
+    </body>
+</html>
+
+<?php 
+echo $_SERVER['DOCUMENT_ROOT'];
+?>
